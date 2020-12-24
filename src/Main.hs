22393f99937c655409150f9090c0033b4e51aa86
risingbirdsong <heregoingthere@outlook.com>
2020-12-23 20:05:43 -0800

    woohoo got random words showing up

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
+import System.Random
 
-main :: IO ()
+type WordList = [String]
+minWordLength = 3
+maxWordLength = 9
+
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "words.txt"
+  return (lines dict)
+
+gameWords :: IO WordList
+gameWords = do 
+  aw <- allWords
+  return (filter gamelength aw)
+  where gamelength w = 
+          let l = length (w :: String)
+          in l > minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIndex <- randomRIO ( 0 , length wl -1 )
+  return $ wl !! randomIndex
+
+
+-- test = ran 
+-- main :: IO WordList
 main = do
-  putStrLn "hello world"
+  allwords <- gameWords
+  randomWord <- randomWord allwords 
+  putStrLn randomWord
+  return ()
